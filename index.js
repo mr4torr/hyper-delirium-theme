176c93886200e3bbfecb949306bd8c91ad351d2b
Mailon Torres <mailontorres@gmail.com>
2020-10-12 11:09:30 -0300

    Custom tab

--- index.js
+++ index.js
@@ -24,48 +24,38 @@
 
 "use strict";
 
+const colors = {
+  black: "#1E262C",
+  white: "#EEEEEE",
+  red: "#F07076",
+  green: "#98c379",
+  yellow: "#e5c07b",
+  blue: "#84B0D8",
+  magenta: "#56b6c2",
+  cyan: "#C8BDDE",
+
+  lightBlack: "#8E949E",
+  lightWhite: "#FFFFFF",
+  lightRed: "#FF9197",
+  lightGreen: "#ABDB88",
+  lightYellow: "#FFD68A",
+  lightBlue: "#94C5F2",
+  lightMagenta: "#60CDDB",
+  lightCyan: "#DFD2F7",
+
+  colorCubes: "#98c379",
+  grayscale: "#787D85",
+};
+
 const BORDER = "#363F47";
 const BACKGROUND = "#263038";
 const FOREGROUND = "#EDF1F2";
-
-const RED = "#F07076";
-const GREEN = "#98c379";
-const YELLOW = "#e5c07b";
-const BLUE = "#84B0D8";
-const CYAN = "#C8BDDE";
-const MAGENTA = "#56b6c2";
-const GRAY = "#787D85";
-const BLACK = "#1E262C";
-const WHITE = "#EEEEEE";
-const GRAYSCALE = "#787D85";
-
-const CURSOR_COLOR = GREEN;
-const BORDER_COLOR = BORDER;
-
 const TAB_BORDER_COLOR = "#343D4A";
-const TAB_TEXT_COLOR = FOREGROUND;
-const TAB_BORDER_ACTIVE_COLOR = "#FFAE57";
 
-const colors = {
-  black: BLACK,
-  red: RED,
-  green: GREEN,
-  yellow: YELLOW,
-  blue: BLUE,
-  magenta: MAGENTA,
-  cyan: CYAN,
-  white: WHITE,
-  lightWhite: "#FFFFFF",
-  lightBlack: GRAY,
-  lightRed: RED,
-  lightGreen: GREEN,
-  lightYellow: YELLOW,
-  lightBlue: BLUE,
-  lightMagenta: MAGENTA,
-  lightCyan: CYAN,
-  colorCubes: GREEN,
-  grayscale: GRAYSCALE,
-};
+const CURSOR_COLOR = colors.colorCubes;
+const BORDER_COLOR = BORDER;
+const TAB_TEXT_COLOR = colors.grayscale;
+const TAB_BORDER_ACTIVE_COLOR = colors.colorCubes;
 
 exports.decorateConfig = (config) => {
   let windowControlsCSS = "";
@@ -79,10 +69,12 @@ exports.decorateConfig = (config) => {
   // tab border customization
   let tabBorder = "";
   let tabNoFirstChild = "";
+
   let headerBorderColor =
     delirium.noBorder === true || delirium.showHeaderBorder === false
       ? BACKGROUND
       : TAB_BORDER_COLOR;
+
   let tabBorderColor =
     delirium.noBorder === true || delirium.showTabBorder === false
       ? BACKGROUND
@@ -97,7 +89,7 @@ exports.decorateConfig = (config) => {
   }
 
   // header customization (windows only)
-  let headerForegroundColor = WHITE;
+  let headerForegroundColor = colors.white;
   let headerBackgroundColor = BACKGROUND;
   if (isWin) {
     headerBackgroundColor =
@@ -112,8 +104,24 @@ exports.decorateConfig = (config) => {
     borderColor: BORDER_COLOR,
     cursorColor: CURSOR_COLOR,
     colors,
+    termCSS: `
+          ${config.termCSS || ""}
+          ::selection {
+              background: #9198A2 !important;
+          }
+          x-screen x-row {
+              font-variant-ligatures: initial;
+          }
+          span {
+              font-weight: normal !important;
+          }
+      `,
     css: `
       ${config.css || ""}
+      ::selection {
+        background: #9198A2 !important;
+      }
+
       .hyper_main {
         border: none !important;
       }
@@ -121,17 +129,17 @@ exports.decorateConfig = (config) => {
         background-color: ${tabBorderColor} !important;
       }
       .header_header {
-        background: transparent !important;
-        border-bottom: none !important;
+        color: ${headerForegroundColor} !important;
       }
       .header_header, .header_windowHeader {
         top: 0;
         left: 0;
         right: 0;
-        color: ${headerForegroundColor} !important;
       }
       .tabs_list {
         position: relative;
+        border: none;
+        border-bottom: 1px solid ${tabBorderColor} !important;
       }
       .tabs_list:before {
         content: '';
@@ -147,34 +155,34 @@ exports.decorateConfig = (config) => {
       }
       .tabs_title {
         color: ${TAB_TEXT_COLOR};
-        font-weight: 600;
       }
       .tab_tab {
         border: 0;
-        ${tabBorder}
-        background-color: rgba(0,0,0,0.1) !important;
       }
       .tab_tab${tabNoFirstChild} {
         border-left: 1px solid ${tabBorderColor} !important;
       }
       .tab_text {
         color: ${TAB_TEXT_COLOR};
+        font-size: 13px;
         font-weight: normal;
       }
       .tab_tab.tab_active {
+        font-size: 13px;
+        color: ${FOREGROUND} !important;
         background-color: transparent !important;
       }
-      .tab_textActive {
-        color: ${TAB_TEXT_COLOR};
-        font-weight: 600;
+      .tab_tab.tab_active:before {
+        content: '';
+        position: absolute;
+        bottom: 0px;
+        left: 0;
+        width: 100%;
+        height: 1px;
+        background-color: ${TAB_BORDER_ACTIVE_COLOR};
       }
-      .tab_icon {
-        color: ${TAB_TEXT_COLOR};
-        font-weight: 600;
-      }
-      .tab_icon:hover {
-        color: ${TAB_TEXT_COLOR};
-        font-weight: 600;
+      .tab_textActive, .tab_icon, .tab_icon:hover {
+        color: ${FOREGROUND};
       }
       ${windowControlsCSS}
     `,
